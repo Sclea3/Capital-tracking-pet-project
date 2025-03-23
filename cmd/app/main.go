373962e60eea41ac9bbd7de9d052f870706d5f0f
Sclea3 <sashamaty30@gmail.com>
2025-03-23 19:50:25 +0500

    set up reading from .env

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	fmt.Println("Starting Capital.com Market Monitor...")
+
+	// TODO: Initialize configuration
+	// TODO: Set up logger
+	// TODO: Create API client
+	// TODO: Initialize storage
+	// TODO: Setup analyzer
+	// TODO: Start data collector
+
+	log.Println("Application running. Press Ctrl+C to stop.")
+
+	// TODO: Add graceful shutdown
+	select {}
+}
